3922a02b8b43403c5f78a622944d8c43a7d029cf
Alex <pallarete@gmail.com>
2025-10-30 12:57:48 +0100

    operadores_comparacion

---
+++ operadores/opera_comparacion.py
@@ -0,0 +1,32 @@
+'''Operadores Comparacion'''
+import os
+os.system('cls')
+igual_que = 5 == 6
+distinto_de = 5 != 6
+mayor_que = 5 > 6
+menor_que = 5 < 6
+mayor_o_igual = 5 >= 6
+menor_o_igual = 5 <= 6
+
+print(igual_que)
+print(distinto_de)
+print(mayor_que)
+print(menor_que)
+print(mayor_o_igual)
+print(menor_o_igual)
+
+'''CALCULOS COMBINADOS'''
+a = 5
+b = 10
+c = 20
+
+comparacion = a + b < c
+
+print(comparacion)
+
+# Comparar usuarios
+
+usuario_de_base_de_datos = 'Alex_Maestro' 
+usuario_escrito ='Alex'
+
+print(usuario_de_base_de_datos == usuario_escrito)
