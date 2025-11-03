19ae5b01329edce09327c5bc6cb54d5acf833ede
Alex <pallarete@gmail.com>
2025-11-03 13:08:32 +0100

    while

---
+++ bucles/bucle_why.py
@@ -0,0 +1,8 @@
+''' BUCLE WHY '''
+import os
+os.system('cls')
+# Iterando con bucle why
+contador =0
+while contador < 10:
+    print(contador)
+    contador +=1
--- bucles/mas_iteraciones.py
+++ bucles/mas_iteraciones.py
@@ -2,6 +2,30 @@
 import os
 os.system('cls')
 frutas = ['banana','manzana','ciruela','pera','naranaja','granada','durazno']
-
+cadena ='Hola Alex'
+numeros = [2, 5, 8, 10]
+#Saltando un iteracion con el continue bajo una condicion
 for fruta in frutas:
+    if fruta =='granada':
+        continue
     print(   f'Me voy a comer una : {fruta}'  )
+
+#Saliendo del bucle con el break bajo una condicion
+for fruta in frutas:
+    if fruta =='pera':
+        break
+    print(   f'Me voy a comer una : {fruta}'  )
+
+print('\n')
+for letra in cadena:
+    print(letra)
+
+#for en una sola linea de codigo
+numeros_duplicados = list()
+for numero in numeros:
+    numeros_duplicados.append(numero * 2)
+print(numeros_duplicados)
+
+# Lo mismo en una linea de codigo
+numero_duplicado = [x*2 for x in numeros]
+print(numero_duplicado)
